10ccf47591835f09183a81590bfdb0ef37fbdbc2
remi <remi.maringer@gmail.com>
2018-09-10 12:56:35 +0200

    Premier commit

---
+++ src/components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="contact">
+      <h1>Page de contact tavu</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact'
+}
+</script>
+
+<style scoped>
+  .test-title{
+    color:red !important;
+  }
+</style>
+<style>
+
+</style>
